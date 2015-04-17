84a742b45f4ffd4ecf20e7dd5d4485880f52119e
Peter Harrington <pharrington@g.harvard.edu>
2015-04-17 16:14:10 -0700

    finished comments in acronym.py

--- acronym.py
+++ acronym.py
@@ -3,12 +3,19 @@
 
 class Acronym:
 
+  # can be given a directory where the corpus documents are stored.  
+  # The corpus is optional if you want to use this module for online 
+  # processing.  
   def __init__(self, corpus = ""):
     self.corpus = corpus
 
+  # Find all the acronyms in a given string of text.
+  # Retruns a list of the acronyms.
   def extract_acronyms(in_text, self):
     pass
 
+  # Finds any in-line definitions of acronyms.  An example of this is: AI 
+  # (Artificial Intelligence).  Returns a list of acronym, defintion pairs. 
   def extract_acronym_defs(in_text, self):
     pass
 
