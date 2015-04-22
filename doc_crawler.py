756755100a76bf67caac69cfd782f4182de6bfbb
Peter Harrington <pharrington@g.harvard.edu>
2015-04-22 06:47:35 -0700

    doc crawler class added

--- doc_crawler.py
+++ doc_crawler.py
@@ -4,3 +4,13 @@
 # http://www.simplyhired.com/search?q=EMR
 # produces a "corpus" in the corpus folder of plan text documents.
 # this only needs to be run one time.  
+
+class DocCrawler():
+  def __init__(corpus, self):
+    self.corpus = corpus
+
+  
+  
+if __name__ == "__main__":
+
+  print "crawling"
