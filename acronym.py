b678a1b31f4cf2162f403fc22b53fdfd8da35240
Peter Harrington <pharrington@g.harvard.edu>
2015-04-23 15:11:10 -0700

    finished acronym def extraction and added tests

--- acronym.py
+++ acronym.py
@@ -3,6 +3,7 @@
 import re
 
 CONTEXT_W = 15  # number of words on both sides of acronym for context
+ACRONYM_REGEX = re.compile(r'((?:[A-Z]\.){2,}|[A-Z]{2,})\s+\([A-Za-z ]+\)')
 
 class Acronym:
 
@@ -12,20 +13,34 @@ class Acronym:
   def __init__(self, corpus = ""):
     self.corpus = corpus
 
+  def __remove_periods__(self, instr):
+    return instr.replace(".", "")
+
   # Find all the acronyms in a given string of text.
   # Retruns a list of the acronyms.
-  def extract_acronyms(in_text, self):
+  def extract_acronyms(self, in_text):
     # regular expressions should match: CMS or C.M.S.
-    return re.findall(r'(?:[A-Z]\.){2,}|[A-Z]{2,}', in_text)
+    matches = re.findall(r'(?:[A-Z]\.){2,}|[A-Z]{2,}', in_text)
+    return map(self.__remove_periods__, matches)
 
   # Finds any in-line definitions of acronyms.  An example of this is: AI 
   # (Artificial Intelligence).  Returns a list of acronym, defintion pairs. 
-  def extract_acronym_defs(in_text, self):
-    # regular expressions should match: CMS (Content Management System) 
-    pass
+  def extract_acronym_defs(self, in_text):
+    r = []
+    match = ACRONYM_REGEX.search(in_text, 0)
+    while match != None:
+      ac_meaning_str = match.group()
+      ac = match.groups()[0]                         # get acronym matched
+      meaning_str = ac_meaning_str[len(ac):].strip() # remove acronym str
+      meaning_str = meaning_str[1:-1]                # remove parens
+      r.append( (ac.replace(".", ""), meaning_str) )
+      match = ACRONYM_REGEX.search(in_text, match.end())
+
+    return r
+
 
   # given a valid corpus, return a set of words commonly "near" the acronym 
-  def harvest_context(in_text, acronym_str, self):
+  def harvest_context(self, in_text, acronym_str):
     pass
 
   # see below for pseudo code
@@ -34,18 +49,18 @@ class Acronym:
 
   # iterate over the documents in the corpus and calculate the popularity
   # the return value is a float.
-  def calc_acronym_popularity(acronym_str, doc_arr, self):
+  def calc_acronym_popularity(self, acronym_str, doc_arr):
     pass
   
   # iterate over the dict of acronym dicts and merge meanings that are close.
   # There is nothing returned as you can modify dicts in place in Python.  
-  def deduplicate(in_dict, self):
+  def deduplicate(self, in_dict):
     pass
 
   # Given an acronym_str and a context decided the best possible meaning. 
   # Return a list of dicts with scores, the list should be sorted from 
   # highest to lowest score.  
-  def rank_meanings(acronym_str, context, self):
+  def rank_meanings(self, acronym_str, context):
     pass
 
   # see below for pseudo code
@@ -74,7 +89,31 @@ class Acronym:
 
     # serialze data dict with acronyms, in JSON so other apps can load it. 
     pass
+
+  def test_acronym_extraction(self):
+
+    test_text = "I am listening to RR (Ryan Renolds)" 
+    acs = m.extract_acronyms(test_text)
+    assert acs == ["RR"], "failed simple extract acronym"
+
+    test_text = "I am listening to R.R. (Rick Ross)"
+    acs = m.extract_acronyms(test_text)
+    assert acs == ["RR"], "failed to extract acronym with periods"
+
+  def test_extract_acronym_defs(self):
+    test_text = "I am listening to R.R. (Rick Ross)"
+    meaning = m.extract_acronym_defs(test_text)
+    assert meaning == [("RR", "Rick Ross")], "failed simple meaning extraction"
+
+    test_text = "I am listening to RR (Ryan Renolds) and R.R. (Rick Ross)" 
+    meaning = m.extract_acronym_defs(test_text)
+    assert meaning == [("RR", "Ryan Renolds"), ("RR", "Rick Ross")], 
+         "failed multiple meaning extraction"
   
 if __name__ == "__main__":
+  m = Acronym()
 
-  print "working on it bro"
+  print "running tests"
+  m.test_acronym_extraction()
+  m.test_extract_acronym_defs()
+
